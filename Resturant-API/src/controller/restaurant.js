241bd2c9f1f27fbe418f9d4c66e19256add3547a
Noel-F <2591142+Noel-F@users.noreply.github.com>
2017-10-07 08:01:48 -0400

    Added C.R.U.D
    - Created functions to create, read, update and delete items from the database.

--- Resturant-API/src/controller/restaurant.js
+++ Resturant-API/src/controller/restaurant.js
@@ -5,7 +5,10 @@ import Restaurant from '../model/restaurant';
 export default({config, db}) => {
   let api = Router();
 
-  //: '/v1/resturant/add'
+  //: C.R.U.D
+
+
+  //: /v1/resturant/add - Create
   api.post('/add', (req, res) => {
     let newRest = new Restaurant();
     newRest.name = req.body.name;
@@ -15,6 +18,54 @@ export default({config, db}) => {
         res.send(err);
       }
       res.json({message: 'Restaurant saved successfully!'});
+    });
+  });
+
+  //: /v1/resturant - Read: Returns all documents.
+  api.get('/', (req, res) => {
+    Restaurant.find({}, (err,  resturants) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(resturants);
+    });
+  });
+
+  //: /v1/resturant/:id - Read: returns document based on ID.
+  api.get('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, resturant) => {
+      if (err) {
+        res.send(err)
+      }
+      res.json(resturant);
+    });
+  });
+
+  //: /v1/resturant/:id - Update
+  api.put('/:id', (req, res) => {
+    Restaurant.findById(req.params.id, (err, resturant) => {
+      if (err) {
+        res.send(err);
+      }
+      resturant.name = req.body.name;
+      resturant.save(err => {
+        if (err) {
+          res.send(err);
+        }
+        res.json({message: "Restaurant info updated!"});
+      });
+    });
+  });
+
+  //: /v1/resturant/:id - Delete
+  api.delete('/:id', (req, res) => {
+    Restaurant.remove({
+      _id: req.params.id
+    }, (err, restaurant) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({message: "Resturant successfully removed!"});
     });
   });
 
